92afe798ee5bdf1bb609ef4d3daa7a0dd31b90d3
Thomas Kreuzer <thomas.kreuzer@uni-wuerzburg.de>
2025-03-05 10:22:02 +0100

    Outcast

--- examples/ReadSample/ReadSample.ino
+++ examples/ReadSample/ReadSample.ino
@@ -1,9 +1,20 @@
 #include <Wire.h>
 #include <SHTC3.h>
 
+void printReadings() {
+  Serial.print("Temperature: ");
+  Serial.print(SHTC3.getTemperature());
+  Serial.println(" °C");
+
+  Serial.print("Humidity: ");
+  Serial.print(SHTC3.getHumidity());
+  Serial.println(" %RH");
+}
+
 void setup() {
   Serial.begin(9600);
   while (!Serial);
+  delay(3000);
 
   Wire.begin();
   if(!SHTC3.begin()) {
@@ -14,15 +25,32 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("Normal Mode, Clock Stretching Enabled, Read T First");
   SHTC3.readSample();
+  printReadings();
+  Serial.println();
 
-  Serial.print("Temperature: ");
-  Serial.print(SHTC3.getTemperature());
-  Serial.println(" °C");
+  delay(5000);
 
-  Serial.print("Humidity: ");
-  Serial.print(SHTC3.getHumidity());
-  Serial.println(" %RH");
+  Serial.println("Normal Mode, Clock Stretching Disabled, Read T First");
+  SHTC3.readSample(false);
+  printReadings();
+  Serial.println();
+
+  delay(5000);
+
+  Serial.println("Low Power M., Clock Stretching Enabled, Read T First");
+  SHTC3.readSample(true, true);
+  printReadings();
+  Serial.println();
+
+  delay(5000);
+
+  Serial.println("Low Power M., Clock Stretching Disabled, Read T First");
+  SHTC3.readSample(false, true);
+  printReadings();
+  Serial.println();
+  Serial.println("----");
 
   delay(5000);
 }
--- src/SHTC3.cpp
+++ src/SHTC3.cpp
@@ -5,6 +5,11 @@
 
 #define I2C_ADDR 0x70
 
+const uint16_t MCMD[2][4] = {
+  {0x7866, 0x58E0, 0x7CA2, 0x5C24},
+  {0x609C, 0x401A, 0x6458, 0x44DE}
+};
+
 _SHTC3 SHTC3;
 
 byte _crc8(byte data[], int len) {
@@ -21,20 +26,24 @@ byte _crc8(byte data[], int len) {
   return crc;
 }
 
-bool _recv(uint16_t *j) {
+bool _recv(uint16_t *j, int len = 1) {
   byte buf[3];
 
-  Wire.requestFrom(I2C_ADDR, 3, false);
-  for (int i = 0; Wire.available() && i < 3; i++)
+  Wire.requestFrom(I2C_ADDR, 3*len, false);
+  for (int k = 0; k < len; k++) {
+    for (int i = 0; i < 3; i++) {
+      while (!Wire.available())
+        delay(1);
       buf[i] = Wire.read();
+    }
+    if (buf[2] != _crc8(buf, 2))
+      return false;
 
-  if (buf[2] != _crc8(buf, 2))
-    return false;
-
-  if (j != NULL) {
-    *j = buf[0];
-    *j <<= 8;
-    *j |= buf[1];
+    if (j != nullptr) {
+      j[k] = buf[0];
+      j[k] <<= 8;
+      j[k] |= buf[1];
+    }
   }
   return true;
 }
@@ -68,38 +77,32 @@ float _SHTC3::getTemperature() {
 }
 
 void _SHTC3::readSample(bool clock_stretch, bool low_power, bool rh_first) {
-  const uint16_t cmd[2][4] = {
-    {0x7866, 0x58E0, 0x7CA2, 0x5C24},
-    {0x609C, 0x401A, 0x6458, 0x44DE}
-  };
-
   int i = (int)low_power;
   int j = (int)rh_first;
   int k = (int)clock_stretch * 2;
 
   wakeup();
   delay(1);
-  _send(cmd[i][j+k]);
+  _send(MCMD[i][j+k]);
   if (!clock_stretch)
-    delay(low_power ? 2 : 20);
-  uint16_t a, b;
-  _recv(&a);
-  _recv(&b);
+    delay(low_power ? 2 : 14);
+  uint16_t a[2];
+  _recv(a, 2);
   sleep();
 
   if (rh_first) {
-    uint16_t c = b;
-    b = a;
-    a = c;
+    uint16_t c = a[0];
+    a[0] = a[1];
+    a[1] = c;
   }
 
-  _st = 175.0 * a / (1<<16) - 45.0;
-  _srh = 100.0 * b / (1<<16);
+  _st = 175.0 * a[0] / (1<<16) - 45.0;
+  _srh = 100.0 * a[1] / (1<<16);
 }
 
 bool _SHTC3::ready() {
   _send(0xEFC8);
-  return _recv(NULL);
+  return _recv(nullptr);
 }
 
 void _SHTC3::reset() {
